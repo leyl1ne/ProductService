906d88d905a4522bcdf1ab02da1608b0756de745
leyl1ne <d-khodin@inbox.ru>
2026-06-04 12:12:39 +0300

    feat: add http server config struct

---
+++ pkg/httpserver/config.go
@@ -0,0 +1,23 @@
+package httpserver
+
+import "time"
+
+type Config struct {
+	HTTP HTTP `yaml:"http"`
+
+	GracefulShutdown time.Duration `yaml:"graceful_shutdown"  env:"SERVER_GRACEFUL_SHUTDOWN"`
+}
+
+type BasicAuth struct {
+	Enabled  bool   `yaml:"enabled"   env:"SERVER_HTTP_BASICAUTH_ENABLED"`
+	Username string `yaml:"username"  env:"SERVER_HTTP_BASICAUTH_USERNAME"`
+	Password string `env:"SERVER_HTTP_BASICAUTH_PASSWORD"`
+}
+
+type HTTP struct {
+	Port         int           `yaml:"port"          env:"SERVER_HTTP_PORT"`
+	ReadTimeout  time.Duration `yaml:"read_timeout"  env:"SERVER_HTTP_READ_TIMEOUT"`
+	WriteTimeout time.Duration `yaml:"write_timeout" env:"SERVER_HTTP_WRITE_TIMEOUT"`
+
+	BasicAuth BasicAuth `yaml:"basic_auth"`
+}
