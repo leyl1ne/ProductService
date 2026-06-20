21747abb4c89fb07a3684ed303a415a938be57f7
leyl1ne <d-khodin@inbox.ru>
2026-06-20 16:35:59 +0300

    feat(category): add error

--- internal/model/category/errors.go
+++ internal/model/category/errors.go
@@ -3,6 +3,9 @@ package category
 import "errors"
 
 var (
-	ErrNameIsReuired = errors.New("category name is required")
-	ErrNameIsTooLong = errors.New("category name is too long")
+	ErrNameIsReuired         = errors.New("category name is required")
+	ErrNameIsTooLong         = errors.New("category name is too long")
+	ErrCategoryAlreadyExists = errors.New("category already exists")
+	ErrCategoryNotFound      = errors.New("category not found")
+	ErrCategoryInUse         = errors.New("category is in use by products")
 )
