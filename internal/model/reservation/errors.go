e496aa8683adab4985949a7c7427f8ec26fe081a
leyl1ne <d-khodin@inbox.ru>
2026-06-20 16:36:55 +0300

    feat(reservation): add error

--- internal/model/reservation/errors.go
+++ internal/model/reservation/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrOrderIdIsRequired = errors.New("order id is required")
 	ErrBatchIDIsRequired = errors.New("batch id is required")
 	ErrInvalidQuantity   = errors.New("quantity must be greater than zero")
+	ErrInsufficientStock = errors.New("insufficient stock available")
 )
